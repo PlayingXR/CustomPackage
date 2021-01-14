389da6492ac5749446d75b392e63d0d3dc8a1087
wuxiaohua <wuxiaohua@youcheyihou.com>
2021-01-14 14:56:43 +0800

    .

---
+++ Runtime/UIView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UIKit.Runtime
+{
+    public class UIView: UIBehaviour
+    {
+        public void Init()
+        {
+            Debug.Log("UIView did inited!");
+        }
+    }
+}
