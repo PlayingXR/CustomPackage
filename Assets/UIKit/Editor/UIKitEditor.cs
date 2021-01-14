19defbe9aff97e308abc6fec47fb6b5224d70391
wuxiaohua <wuxiaohua@youcheyihou.com>
2021-01-14 14:56:43 +0800

    .

---
+++ Assets/UIKit/Editor/UIKitEditor.cs
@@ -0,0 +1,19 @@
+﻿using UIKit.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UIKit.Editor
+{
+    public class UIKitEditor
+    {
+        [MenuItem("UIKit/Test")]
+        public static void Test()
+        {
+            UITest test = new UITest();
+            test.Init();
+            Debug.Log("Test");
+        }
+    }
+}
+
+
---
+++ Assets/UIKit/Runtime/UITest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UIKit.Runtime
+{
+    public class UITest
+    {
+        public void Init()
+        {
+            Debug.Log("UITest did inited!");
+        }
+    }
+}
